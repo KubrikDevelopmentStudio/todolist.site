6d598273d39b769783719d77f7da21ceebf6d37c
Ruslan Alfimov <alfimov95@mail.ru>
2016-11-18 22:11:46 +0300

    start project

---
+++ functions/handler.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18.11.16
+ * Time: 19:57
+ */
+
+switch ($_REQUEST['action']) {
+    case 'getUser':
+        break;
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18.11.16
+ * Time: 19:56
+ */
+
+?>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+                Panel content
+            </div>
+            <div class="panel-footer">Panel footer</div>
+        </div>
+    </body>
+</html>
