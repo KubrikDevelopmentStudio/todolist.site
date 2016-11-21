b7019afd7ad57d86e8f694cd948a95c274d917cd
Ruslan Alfimov <r.alfimov@newcontact.su>
2016-11-21 13:17:29 +0300

    переходим на font-awesome

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <html>
     <head>
         <link href="lib/bootstrap4/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+
+        <script type="text/javascript" src="lib/jquery/jquery-3.1.1.js"></script>
+        <script type="text/javascript" src="lib/bootstrap4/js/bootstrap.js"></script>
     </head>
     <body>
     <form enctype="multipart/form-data" method="post" action="">
@@ -22,26 +26,48 @@
                <h4 class="card-title">Авторизация</h4>
 
                 <div class="input-group">
-                    <span class="input-group-addon" id="basic-addon1">
-                         <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                    <span class="input-group-addon" id="sizing-addon2">
+                        <i class="fa fa-user-o" aria-hidden="true"></i>
                     </span>
-                    <input type="text" name="user" class="form-control" placeholder="Ваш логин" aria-describedby="basic-addon1">
+
+                    <input type="text" name="user" class="form-control" placeholder="Ваш логин" aria-describedby="sizing-addon2">
                 </div>
                 <br>
                 <div class="input-group">
-                    <span class="input-group-addon" id="basic-addon1">
-                         <span class="glyphicon glyphicon-lock" aria-hidden="true"></span>
+                    <span class="input-group-addon" id="sizing-addon2">
+                         <i class="fa fa-lock" aria-hidden="true"></i>
                     </span>
-                    <input type="password" name="pass" class="form-control" placeholder="Ваш пароль" aria-describedby="basic-addon1">
+                    <input type="password" name="pass" class="form-control" placeholder="Ваш пароль" aria-describedby="sizing-addon2">
                 </div>
                 <br>
                 <div class="input-group-btn">
-                    <button type="submit" class="btn btn-primary" style="width: 100%; height: auto; margin: 5px auto; border-radius: 3px">Войти</button>
+                    <button type="submit" class="btn btn-outline-primary" style="width: 100%; height: auto; margin: 5px auto; border-radius: 3px">Войти</button>
                 </div>
 
+
+
+               <div class="modal fade" id="info" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                   <div class="modal-dialog" role="document">
+                       <div class="modal-content">
+                           <div class="modal-header">
+                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                   <span aria-hidden="true">&times;</span>
+                               </button>
+                               <h4 class="modal-title" id="myModalLabel">Справка с помощью</h4>
+                           </div>
+                           <div class="modal-body">
+                               Вам уже ни что не поможет...
+                           </div>
+                           <div class="modal-footer">
+                               <button type="button" class="btn btn-secondary" data-dismiss="modal">Спасибо</button>
+                               <!--<button type="button" class="btn btn-primary">Save changes</button>-->
+                           </div>
+                       </div>
+                   </div>
+               </div>
             </div>
             <div class="card-footer text-muted">
-                Пожалуйста авторизируйтесь!
+                <a href="#"  data-toggle="modal" data-target="#info">Необходима помощь?</a>
             </div>
         </div>
     </form>
